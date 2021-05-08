b23160774bf13c6408cfd8dc84844c6495e5e7d0
Oscar Morales <oscar.s.morales.z@usp.br>
2021-05-08 15:44:30 -0300

    Modificar janela pop-up

--- js/jsIndex.js
+++ js/jsIndex.js
@@ -8,14 +8,20 @@
 //     muestra_oculta('contenido');/* "contenido_a_mostrar" es el nombre que le dimos al DIV */
 //     }
 
-function show_hide_header_nav__container(id) {
+function show_hide_header_nav__container(id1, id2, id3, id4) {
     if (document.getElementById) {
-        var el = document.getElementById(id);
-        el.style.display = (el.style.display == 'block') ? 'none' : 'block';
+        var el1 = document.getElementById(id1);
+        var el2 = document.getElementById(id2);
+        var el3 = document.getElementById(id3);
+        var el4 = document.getElementById(id4);
+        el1.style.display = (el1.style.display == 'block') ? 'none' : 'block';
+        el2.style.display = (el2.style.display == 'block') ? 'none' : 'none';
+        el3.style.display = (el3.style.display == 'none') ? 'block' : 'block';
+        el4.style.display = (el4.style.display == 'block') ? 'none' : 'none';
     }
 }
 window.onload = function () {
-    show_hide_header_nav__container('id-header-nav--container');
+    show_hide_header_nav__container('id-header-nav--container', 'id-header-nav-container--submenu', 'id-header-nav--down-arrow', 'id-header-nav--up-arrow');
 }
 
 function show_header_nav_container__submenu(id1, id2,id3) {
