a5adcdc58062782e19dfc793dcbcc667091a39af
Oscar Morales <oscar.s.morales.z@usp.br>
2021-05-10 18:23:44 -0300

    Agregar seccion y funciones

---
+++ js/ingenieria.js
@@ -0,0 +1,11 @@
+// function show_information(id1,id2) {
+//     if (document.getElementById) {
+//         var el1 = document.getElementById(id1);
+//         var el2 = document.getElementById(id2);
+//         el1.style.display = (el1.style.display == 'block') ? 'none' : 'block';
+//         el2.style.display = (el2.style.display == 'none') ? 'block' : 'none';
+//     }
+// }
+// window.onload = function () {
+//     show_information('id-main-container-service--title-2');
+// }
