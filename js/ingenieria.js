fa7617fa49be1266f827018cd953b4d7c3ed66b1
ankale <angievalencas@gmail.com>
2021-05-11 00:10:04 -0300

    Se agrego el codigo en java para modificar los containers en servicio de ingenieria

--- js/ingenieria.js
+++ js/ingenieria.js
@@ -1,11 +1,21 @@
-// function show_information(id1,id2) {
-//     if (document.getElementById) {
-//         var el1 = document.getElementById(id1);
-//         var el2 = document.getElementById(id2);
-//         el1.style.display = (el1.style.display == 'block') ? 'none' : 'block';
-//         el2.style.display = (el2.style.display == 'none') ? 'block' : 'none';
-//     }
-// }
-// window.onload = function () {
-//     show_information('id-main-container-service--title-2');
-// }
+var image = ['down','down'];
+function muestra_oculta(id1,id2){
+    if (document.getElementById){ //se obtiene el id
+        var el1 = document.getElementById(id1);
+        var el2 = document.getElementById(id2);
+        el1.style.display = (el1.style.display == 'none') ? 'block' : 'none';
+        var containerNum = parseInt(id1.charAt(id1.length-1));
+        // console.log(image[containerNum-1])
+        if (image[containerNum-1] == 'up'){
+            el2.style.backgroundImage = "url('../assets/icons/UpArrowBlue.svg')"
+            image[containerNum-1]= 'down';
+        } else if (image[containerNum-1] == 'down'){
+            el2.style.backgroundImage = "url('../assets/icons/DownArrowBlue.svg')"
+            image[containerNum-1] = 'up';
+        }
+    }
+}
+window.onload = function () {
+    muestra_oculta('id-contenido1','id-arrow-up1');
+    muestra_oculta('id-contenido2','id-arrow-up2');
+}
