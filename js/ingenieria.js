b160944c7cf2916dbb51a4e72ad59f2f287f9380
ankale <angievalencas@gmail.com>
2021-05-11 10:56:43 -0300

    Se adicionaron containers en el servicio de ingenieria

--- js/ingenieria.js
+++ js/ingenieria.js
@@ -1,11 +1,11 @@
-var image = ['down','down'];
+var image = ['down','down','down','down','down','down','down','down'];
 function muestra_oculta(id1,id2){
     if (document.getElementById){ //se obtiene el id
         var el1 = document.getElementById(id1);
         var el2 = document.getElementById(id2);
         el1.style.display = (el1.style.display == 'none') ? 'block' : 'none';
-        var containerNum = parseInt(id1.charAt(id1.length-1));
-        // console.log(image[containerNum-1])
+        var containerNum = parseInt(id1.slice(-2));
+        // console.log(containerNum)
         if (image[containerNum-1] == 'up'){
             el2.style.backgroundImage = "url('../assets/icons/UpArrowBlue.svg')"
             image[containerNum-1]= 'down';
@@ -16,6 +16,12 @@ function muestra_oculta(id1,id2){
     }
 }
 window.onload = function () {
-    muestra_oculta('id-contenido1','id-arrow-up1');
-    muestra_oculta('id-contenido2','id-arrow-up2');
+    muestra_oculta('id-contenido01','id-arrow-01');
+    muestra_oculta('id-contenido02','id-arrow-02');
+    muestra_oculta('id-contenido03','id-arrow-03');
+    muestra_oculta('id-contenido04','id-arrow-04');
+    muestra_oculta('id-contenido05','id-arrow-05');
+    muestra_oculta('id-contenido06','id-arrow-06');
+    muestra_oculta('id-contenido07','id-arrow-07');
+    muestra_oculta('id-contenido08','id-arrow-08');
 }
