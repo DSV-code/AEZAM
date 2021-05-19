27faf7d479ce7f57fd4bf76f3afbb5d0bef23586
Oscar Morales <oscar.s.morales.z@usp.br>
2021-05-19 18:01:26 -0300

    Add .js goBack and goTop

--- js/contactanos.js
+++
@@ -1,8 +0,0 @@
-var pruebaback = document.getElementById('goBack');
-pruebaback.addEventListener("click", goBack);
-
-function goBack() {
-    window.history.back();
-}
-window.onload = function () {
-}
---
+++ js/goTop.js
@@ -0,0 +1,8 @@
+var gotop = document.getElementById('goTop');
+gotop.addEventListener("click", goTop);
+
+function goTop() {
+    window.scrollTo(0,0);
+}
+window.onload = function () {
+}
